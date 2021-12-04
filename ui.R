37367f3a8c03c79e560f2e882eef0e6db41dec66
Ethanwang17 <ejw17@uw.edu>
2021-12-03 20:45:50 -0800

    Edits

---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+# Define UI ----
+ui <- navbarPage("INFO Project",
+                 tabPanel("Introduction",
+                          h1("Summary"),
+                          p("Health is an essential part of every being and it's important that we humans know how to keep ourselves healthy as good health is critical for one's happieness and well-being. Health is imporatnt as it is essential for keeping our bodies alive and working. This projects goal is to better understand how ")),
+                 tabPanel("Chart 1"),
+                 tabPanel("Chart 2"),
+                 tabPanel("Chart 3"),
+                 tabPanel("Summary",
+                          p("")),
+)
+
+
+
+
