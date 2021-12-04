c5556ed0c16550772f0766c8ce12b23e586ad105
Ethanwang17 <ejw17@uw.edu>
2021-12-03 21:24:05 -0800

    Edits

--- ui.R
+++ ui.R
@@ -4,11 +4,17 @@ library(shiny)
 ui <- navbarPage("INFO Project",
                  tabPanel("Introduction",
                           h1("Summary"),
-                          p("Health is an essential part of every being and it's important that we humans know how to keep ourselves healthy as good health is critical for one's happieness and well-being. Health is imporatnt as it is essential for keeping our bodies alive and working. This projects goal is to better understand how ")),
+                          p("Health is an essential part of every being and it's important that we humans know how to keep ourselves healthy as good health is critical for one's happieness and well-being. Health is imporatnt as it is essential for keeping our bodies alive and working. This projects goal is to better understand how"),
+                          h2("Page Description"),
+                          p("The chart 1 tab is an interactive slider that allows you to filter a range of age groups that shows the resting blood pressure for that rage af ages. This tab explains how the age of a person effects the resting blood pressure of the human body?"),
+                          p("The chart 2 tab allows the user to input their age and sex and will return the average resting blood pressure, cholesterol, and max heart rate between those who do and don't have a heart diesease. This tab explains how does the average resting blood pressure, cholesterol, and max heart rate differ between those who do and don't have a heart disease?"),
+                          p("The chart 3 tab allows the user to choose the pain type and what they want to compare it to and will be shown a bar chart of their selection. This tabe explains how the different types of chest pain (ASY, ATA, NAP, TA) effect the average cholesterol in a person?")
+                          ),
                  tabPanel("Chart 1"),
                  tabPanel("Chart 2"),
                  tabPanel("Chart 3"),
                  tabPanel("Summary",
+                          h1("Major Takeaways"),
                           p("")),
 )
 
