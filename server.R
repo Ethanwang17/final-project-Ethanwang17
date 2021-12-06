74e67e59ff4694084f08564733e7de1930f6cdc5
HHT03 <hht03@uw.edu>
2021-12-05 17:02:35 -0800

    chart 1 putted in

--- server.R
+++ server.R
@@ -1,4 +1,28 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(plotly)
+
+heart_df <- read.csv("heart.csv")
+
+all_age <- heart_df %>%
+  select(Age) %>%
+  unique()
+
 # Define server logic ----
 server <- function(input, output) {
+  
+  output$plot_1 <- renderPlot({
+    options(scipen=10000)
+    
+    chat_1_data <- heart_df %>%
+      filter(country == input$age_type)
+    
+    heart_plot <- ggplot(chart_1_df) +
+      geom_point(mapping = aes(x = input$age_type, y = RestingBP)) +
+      labs(x = input$age_type, "vs", y = "Resting BP", title = "Age Vs Resting BP")
+    
+  })
   
 }
